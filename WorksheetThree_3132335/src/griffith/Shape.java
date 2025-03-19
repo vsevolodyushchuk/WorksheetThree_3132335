0cdea6a52958c11efd95a41eb5df2ad90902bcca
Vsevolod Yushchuk <Sevayushchuk@gmail.com>
2025-03-19 22:27:08 +0000

    Added abstract class Shape with no implementations.

--- WorksheetThree_3132335/src/griffith/Shape.java
+++ WorksheetThree_3132335/src/griffith/Shape.java
@@ -1,5 +1,27 @@
 package griffith;
 
-abstract class Shape {
-	
+public abstract class Shape {
+ private String name;
+
+ public Shape(String name) {
+     this.name = name;
+ }
+
+ public String getName() {
+     return name;
+ }
+
+ public void setName(String name) {
+     this.name = name;
+ }
+
+ public abstract double area();
+ public abstract double perimeter();
+
+ @Override
+ public String toString() {
+     return "Shape: " + name;
+ }
 }
+
+
