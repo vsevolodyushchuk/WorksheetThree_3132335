0e1c8893ab4d7b887d26131639fe8e12d68d6763
Vsevolod Yushchuk <Sevayushchuk@gmail.com>
2025-03-20 00:08:54 +0000

    RightAngledTriangle testing added

--- WorksheetThree_3132335/test/griffith/ShapeTest.java
+++ WorksheetThree_3132335/test/griffith/ShapeTest.java
@@ -23,6 +23,14 @@ public class ShapeTest {
      assertEquals(4 * 6.23, rhombus.perimeter(), EPSILON);
      assertEquals("Shape: Rhombus, Side: 6.23, Height: 8.75", rhombus.toString());
  }
+ 
+ @Test
+ public void testRightAngledTriangle() {
+     RightAngledTriangle triangle = new RightAngledTriangle("Triangle", 3, 4);
+     assertEquals((3 * 4) / 2.0, triangle.area(), EPSILON);
+     assertEquals(3 + 4 + 5, triangle.perimeter(), EPSILON); // Hypotenuse is 5
+     assertEquals("Shape: Triangle, Base: 3, Height: 4", triangle.toString());
+ }
 
 
 }
